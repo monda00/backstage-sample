3444bb10272a2cea3c9c71bf7ad2d78782ed7c9c
monda00 <monda0524@gmail.com>
2024-04-09 22:55:09 +0900

    Update to separate into frontend and backend

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -10,7 +10,7 @@ import { createBackend } from '@backstage/backend-defaults';
 
 const backend = createBackend();
 
-backend.add(import('@backstage/plugin-app-backend/alpha'));
+// backend.add(import('@backstage/plugin-app-backend/alpha'));
 backend.add(import('@backstage/plugin-proxy-backend/alpha'));
 backend.add(import('@backstage/plugin-scaffolder-backend/alpha'));
 backend.add(import('@backstage/plugin-techdocs-backend/alpha'));
